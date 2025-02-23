b6d98804a6ecae03ef10ad6c48d007c4919abc6f
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2025-02-23 11:30:31 -0300

    Preparando backend balancete

---
+++ financas/back-end/financas-api/financas-dominio/Entidade/Balancete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace financas_dominio.Entidade
+{
+    public class Balancete
+    {
+        public int IdBalancete { get; set; }
+        public decimal TotalDespesa { get; set; }
+        public decimal TotalReceita { get; set; }
+        public decimal ResultadoGeral { get; set; }
+        public string Periodicidade { get; set; }
+        public string PeriodoInicial { get; set; }
+        public string PeriodoFinal { get; set; }
+        public int IdReceita { get; set; }
+        public int IdDespesa { get; set; }
+        public Receita Receita { get; set; }
+        public Despesa Despesa { get; set; }
+    }
+}
