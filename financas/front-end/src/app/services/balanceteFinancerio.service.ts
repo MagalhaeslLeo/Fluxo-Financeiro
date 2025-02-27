a0671710d9a68ae3b5d3fe26f57b2c74fc181092
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2025-02-27 10:26:58 -0300

    Criando service balancete

---
+++ financas/front-end/src/app/services/balanceteFinancerio.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { ServiceBase } from "../core/serviceBase";
+
+@Injectable({
+  providedIn: "root",
+})
+export class BalanceteFinanceiroService {
+  constructor(private service: ServiceBase) {}
+
+
+  ObterTodos(): Observable<any> {
+    return this.service.get("BalanceteContabil/ObterTodos");
+  }
+
+  ObterPorId(pId: any): Observable<any> {
+    return this.service.get("BalanceteContabil/ObterPorId", { pIdBalancete: pId });
+  }
+
+  PersistirBalancete(pBalanceteVO:any): Observable<any>{
+    return this.service.post("BalanceteContabil/PersistirBalancete", pBalanceteVO);
+  }
+
+  ExcluirBalancete(pId: any): Observable<any>{
+    return this.service.delete("BalanceteContabil/ExcluirBalancete", {pIdBalancete: pId});
+  } 
+
+  ObterBalanceteContabilPorPeriodo(pDescricao:string, pInicial: string, pFinal: string):Observable<any>{
+    return this.service.get("BalanceteContabil/ObterBalanceteContabilPorPeriodo", {pDescricao: pDescricao, pInicial: pInicial, pFinal: pFinal});
+  }
+  
+}
